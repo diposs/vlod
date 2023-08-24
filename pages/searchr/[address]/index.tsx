e95c42d85079236f1c1fb116ffe449e929e1bc8d
diposs <onakunle.dipo@gmail.com>
2023-08-24 15:48:37 +0100

    Create index.tsx

---
+++ pages/searchr/[address]/index.tsx
@@ -0,0 +1,12 @@
+import { useRouter } from 'next/router'
+
+
+
+const Searchresult = () => {
+  const router = useRouter();
+  const profiledid = router.query
+
+    return (
+    );
+};
+export default Searchresult;
