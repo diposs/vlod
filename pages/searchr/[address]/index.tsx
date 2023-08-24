f59daeec8e6b05b3406574be02df15607933f6de
diposs <onakunle.dipo@gmail.com>
2023-08-24 22:00:14 +0100

    Update index.tsx

--- pages/searchr/[address]/index.tsx
+++ pages/searchr/[address]/index.tsx
@@ -1,7 +1,5 @@
 import { useRouter } from 'next/router'
 import {FirstHeader} from '../../../components/header/header1';
-
-
 export default function Searchresult () {
   const router = useRouter();
     return (
