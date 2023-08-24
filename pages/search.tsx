e58c2735a7944bf6d60c93bfa90db698f84f0950
diposs <onakunle.dipo@gmail.com>
2023-08-24 15:37:54 +0100

    Update search.tsx

--- pages/search.tsx
+++ pages/search.tsx
@@ -1,6 +1,10 @@
 import {SearchContainer} from '../components/container/container1'
+import {SecondHeader} from '../components/header/header2'
 export default function Search() {
   return (
+    <>
+      <SecondHeader />
       <SearchContainer />
+    </>
   );
 }
