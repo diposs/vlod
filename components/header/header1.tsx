30d788aee97b43a0f19762bf8d1e282bf26eb0ac
diposs <onakunle.dipo@gmail.com>
2023-08-24 15:19:51 +0100

    Update header1.tsx

--- components/header/header1.tsx
+++ components/header/header1.tsx
@@ -1,12 +1,15 @@
 import { Header } from '@mantine/core';
 import  useStyles  from '../style/header.style'
 import { HeaderContainer } from '../container/HeaderContainer';
+import { MenuGroup } from '../inputs/MenuGroup';
 
 export function  FirstHeader() {
   const { classes } = useStyles();
   return (
 <Header height="0dvh" className={classes.styles21}>
-<HeaderContainer/>
+<HeaderContainer>
+<MenuGroup/>
+</HeaderContainer>
 </Header>
   );
 };  
