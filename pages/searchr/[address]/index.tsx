8c27b42fd76f9a227bc57b90106d09ec968be536
diposs <onakunle.dipo@gmail.com>
2023-08-24 21:37:15 +0100

    Update index.tsx

--- pages/searchr/[address]/index.tsx
+++ pages/searchr/[address]/index.tsx
@@ -2,15 +2,22 @@ import { useRouter } from 'next/router'
 import {FirstHeader} from '../../../components/header/header1';
 
 
-const Searchresult = () => {
-  const router = useRouter();
-  const profiledid = router.query;
-  console.log('dipps',profiledid?.address);
-
+export default function Searchresult ({ data }) {
+  console.log(data);
     return (
       <>
-      <FirstHeader address={profiledid?.address}/>
+      <FirstHeader/>
       </>
     );
 };
 export default Searchresult;
+
+// This gets called on every request
+export async function getServerSideProps() {
+  // Fetch data from external API
+  const router = useRouter();
+  const data = await router.query()
+ 
+  // Pass data to the page via props
+  return { props: { data } }
+}
