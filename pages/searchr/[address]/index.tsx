218b9e6123dd981d635ac5e6a463ceb60f0ba66b
diposs <onakunle.dipo@gmail.com>
2023-08-24 19:31:01 +0100

    Update index.tsx

--- pages/searchr/[address]/index.tsx
+++ pages/searchr/[address]/index.tsx
@@ -5,7 +5,7 @@ import {FirstHeader} from '../../../components/header/header1';
 const Searchresult = () => {
   const router = useRouter();
   const profiledid = router.query;
-  console.log('dipps',profiledid);
+  console.log('dipps',profiledid?.address);
 
     return (
       <>
