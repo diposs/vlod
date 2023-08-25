3a72edc74009b3048903681244cfaa4a63d7bdce
diposs <onakunle.dipo@gmail.com>
2023-08-25 21:54:34 +0100

    Update searchbar.tsx

--- components/inputs/searchbar.tsx
+++ components/inputs/searchbar.tsx
@@ -1,12 +1,14 @@
 import { useForm } from '@mantine/form';
 import { IconSearch } from '@tabler/icons-react';
-import { Input } from '@mantine/core';
+import { Input, LoadingOverlay } from '@mantine/core';
 import { useRouter } from 'next/router';
+import { useDisclosure } from '@mantine/hooks';
 import { Searchiconright } from '../icons/searchIcon'
 interface FormValus {
   search: string | null;
 }
 export function SearchBar() {
+  const [visible, { toggledd }] = useDisclosure(false);
   const form = useForm<FormValus>({
     initialValues: {
       search: null,
@@ -15,16 +17,24 @@ export function SearchBar() {
   const router = useRouter()
   const handleSearch = async(values: FormValus) => {
     if (typeof values.search === 'string'){
-    console.log(values);
-    router.push('/search/'+values.search)
-    form.reset();
-    console.log(values);
+      toggledd();
+      console.log(values);
+      router.push('/search/'+values.search)
+      form.reset();
     } else{
       form.setErrors({ search: 'Search Input Empty'});
       form.errors;
     }
   }
   return (
+    <LoadingOverlay
+      loaderProps={{ size: 'lg', color: 'pink', variant: 'bars' }}
+      overlayOpacity={0.3}
+      overlayBlur={2}
+      overlayColor="#c5c5c5"
+      visible={visible}
+      transitionDuration= {2000}
+    />
     <form onSubmit={form.onSubmit(handleSearch)}  >
     <Input
       icon={< IconSearch size="2.125rem" />}
