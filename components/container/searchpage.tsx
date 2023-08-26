bc748928c1a1e04b08f09b25d493a9c61ed14f34
diposs <onakunle.dipo@gmail.com>
2023-08-26 10:33:15 +0100

    Update searchpage.tsx

--- components/container/searchpage.tsx
+++ components/container/searchpage.tsx
@@ -2,27 +2,29 @@ import { Center } from '@mantine/core';
 import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 import { ethers } from "ethers";
-
-async function PasswordRequirement ({ password }: { password: string }) {
+const etherchecker =  async(data:string) => {
+  var url = 'https://eth-mainnet.blastapi.io/275cbdc6-c032-4075-8897-cc50b0db3fd5';
+  var provider =  await new ethers.providers.JsonRpcProvider(url);
+  const resolver = await provider.getResolver(data);
+  if (resolver != null){
+      const reclaim =  await resolver.getAddress() || 'error';
+      return (
+    <p>{reclaim}</p>
+  );
+  }else{
+      return (
+    <p>Invalid ENS Address</p>
+  );
+    }
+}
+function PasswordRequirement ({ password }: { password: string }) {
   var shrink = password.replace(/\s/g, '');
   var para = password.toLocaleLowerCase();
   let vault = shrink.toLocaleLowerCase();
   var result =  vault.endsWith(".eth");
   var result2 = /(\b0x[a-f0-9]{40}\b)/g.test(para);
   if (result==true) {
-    var url = 'https://eth-mainnet.blastapi.io/275cbdc6-c032-4075-8897-cc50b0db3fd5';
-    var provider =  await new ethers.providers.JsonRpcProvider(url);
-    const resolver = await provider.getResolver(vault);
-    if (resolver != null){
-      const reclaim =  await resolver.getAddress() || 'error';
-      return (
-    <p>{reclaim}</p>
-  );
-    }else{
-      return (
-    <p>Invalid ENS Address</p>
-  );
-    }
+    etherchecker(vault);
   } 
   if(result2 == true && result == false){
     var resp = para.match(/(\b0x[a-f0-9]{40}\b)/g);
