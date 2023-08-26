fc4f04228758bbdfbe134e66ed1aec73dcd141e8
diposs <onakunle.dipo@gmail.com>
2023-08-26 08:18:58 +0100

    Update index.tsx

--- pages/search/[address]/index.tsx
+++ pages/search/[address]/index.tsx
@@ -1,8 +1,10 @@
 import {FirstHeader} from '../../../components/header/header1';
+import {SearchPage} from '../../../components/container/searchpage';
 export default function Searchresult () {
     return (
       <>
-      <FirstHeader/>
+          <FirstHeader/>
+          <SearchPage/>
       </>
     );
 };
