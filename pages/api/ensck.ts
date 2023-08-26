f88334cc5a157795c215a6f7b860b6b6cfc3c31d
diposs <onakunle.dipo@gmail.com>
2023-08-26 12:57:26 +0100

    Update ensck.ts

--- pages/api/ensck.ts
+++ pages/api/ensck.ts
@@ -7,6 +7,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>){
   const data = req.body
+  console.log(data);
   var url = 'https://eth-mainnet.blastapi.io/275cbdc6-c032-4075-8897-cc50b0db3fd5';
   var provider =  await new ethers.providers.JsonRpcProvider(url);
   const resolver = await provider.getResolver(data);
