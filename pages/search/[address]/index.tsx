75f634b779711a930c8d0906fecac56a11177a86
diposs <onakunle.dipo@gmail.com>
2023-08-26 11:15:28 +0100

    Update index.tsx

--- pages/search/[address]/index.tsx
+++ pages/search/[address]/index.tsx
@@ -1,10 +1,85 @@
 import {FirstHeader} from '../../../components/header/header1';
-import {SearchPage} from '../../../components/container/searchpage';
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Center } from '@mantine/core';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+import { ethers } from "ethers";
+type Data = {
+  name: string
+}
+async function handler(req: NextApiRequest,
+  res: NextApiResponse<Data>){
+    const router = useRouter();
+  var url = 'https://eth-mainnet.blastapi.io/275cbdc6-c032-4075-8897-cc50b0db3fd5';
+  var provider =  await new ethers.providers.JsonRpcProvider(url);
+  const resolver = await provider.getResolver(router.query.address);
+  if (resolver != null){
+      const reclaim =  await resolver.getAddress() || 'error';
+  }else{
+   const reclaim =  'Invalid ENS Address';
+    }
+  res.end(reclaim)
+}
+const Etherchecker =  async() => {
+  const url = await handler ();
+      return (
+    <p>{url}</p>
+  );
+    
+}
+function PasswordRequirement ({ password }: { password: string }) {
+  var shrink = password.replace(/\s/g, '');
+  var para = password.toLocaleLowerCase();
+  let vault = shrink.toLocaleLowerCase();
+  var result =  vault.endsWith(".eth");
+  var result2 = /(\b0x[a-f0-9]{40}\b)/g.test(para);
+  if (result==true) {
+    console.log('ddoings');
+    return (
+    <Etherchecker />
+      );
+  } 
+  if(result2 == true && result == false){
+    var resp = para.match(/(\b0x[a-f0-9]{40}\b)/g);
+    if (resp != null){
+      const resulted = resp[0] || 'goat';
+    console.log(resp);
+    return (
+      <>
+    <p>{resulted}</p>
+    <p>eth address</p>
+        </>
+  );
+    } else{
+      <p> ERR WITH SEARCH QUERY TRY AGAIN</p>
+    }
+    
+  } else{
+    return (
+      <>
+    <p>{shrink}</p>
+        </>
+  );
+}
+}
 export default function Searchresult () {
+    const router = useRouter();
+  const [data, setData] = useState('');
+  const result = data.replace(/\s/g, '');
+  useEffect(() => {
+    if (typeof router.query.address === 'string') {
+         setData(router.query.address);
+    }
+  },[router.query.address])
     return (
       <>
           <FirstHeader/>
-          <SearchPage/>
+          <Center h= '97dvh'>
+            <PasswordRequirement password ={data}/>
+        </Center>
       </>
     );
 };
+
+
+
