edcf2f144889bfe50a08b7edde1bb98ec48ab57a
diposs <onakunle.dipo@gmail.com>
2023-08-26 08:16:59 +0100

    Create searchpage.tsx

---
+++ components/container/searchpage.tsx
@@ -0,0 +1,16 @@
+import { Center } from '@mantine/core';
+;
+
+export function SearchPage() {
+  const router = useRouter()
+  useEffect(() => {
+    if (typeof router.query.address === 'string') {
+        const pd = router.query.address;
+    }
+  },[router.query.address])
+    return (
+        <Center h= '97dvh'>
+            <p>{pd || 'goat'}</p>
+        </Center>
+    );
+}
