178adc3fb1811d65075f29499fe9b84e3f98f9bd
diposs <onakunle.dipo@gmail.com>
2023-08-27 17:11:05 +0100

    Update HeaderContainer.tsx

--- components/container/HeaderContainer.tsx
+++ components/container/HeaderContainer.tsx
@@ -7,8 +7,8 @@ import { useForm, hasLength, matchesField  } from '@mantine/form';
 import { ethers } from "ethers";
 import { GsButton, GsLogoutButton } from '../buttons/GSButton';
 import { useAuth, usePolybase, useIsAuthenticated } from "@polybase/react";
-import { secp256k1, aescbc, decodeFromString, encodeToString, EncryptedDataAesCbc256 } from '@polybase/util'
-import { useBoundStore3} from '../../stores/datastate'
+import { secp256k1, aescbc, decodeFromString, encodeToString, EncryptedDataAesCbc256 } from '@polybase/util';
+import { useBoundStore3} from '../../stores/datastate';
 //import { ethPersonalSign } from '@polybase/eth'
 //import { Polybase } from "@polybase/client"
 import { useEffect } from 'react';
